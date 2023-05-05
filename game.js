c3bab03ebf610b7dbe23512c25a5447dd88e43b3
RetroBowl <99912851+loserboysonyt@users.noreply.github.com>
2023-05-04 19:43:19 -0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>My Game</title>
+    <style>
+      canvas {
+        border: 1px solid black;
+      }
+    </style>
+  </head>
+  <body>
+    <canvas id="canvas" width="400" height="300"></canvas>
+    <script src="game.js"></script>
+  </body>
+</html>
